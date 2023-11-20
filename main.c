da86580ec6dbc4b407c4fe05264b69bb928628ca
AlanHsu <hgodalan@gmail.com>
2023-11-20 20:10:22 +0800

    try other version

--- main.c
+++ main.c
@@ -3,75 +3,64 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/epoll.h>
+#include <sys/wait.h>
 
 #define BUFFER_SIZE 1024
 
-int webConnection()
+void proxyConnection(int dst, int src)
 {
-    // Dial to device web server
-    int webConn = socket(AF_INET, SOCK_STREAM, 0);
-    if (webConn == -1)
+    // Start remote -> local data transfer
+    pid_t remoteToLocal = fork();
+    if (remoteToLocal == 0)
     {
-        perror("Error creating webConn socket");
-        // exit(EXIT_FAILURE);
-        return -1;
+        char buffer[BUFFER_SIZE];
+        ssize_t bytesRead;
+
+        while ((bytesRead = read(src, buffer, sizeof(buffer))) > 0)
+        {
+            write(dst, buffer, bytesRead);
+        }
+
+        close(src);
+        close(dst);
+        exit(EXIT_SUCCESS);
+    }
+    else if (remoteToLocal == -1)
+    {
+        perror("Error creating remoteToLocal process");
+        exit(EXIT_FAILURE);
     }
 
-    struct sockaddr_in webServerAddr;
-    memset(&webServerAddr, 0, sizeof(webServerAddr));
-    webServerAddr.sin_family = AF_INET;
-    webServerAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    webServerAddr.sin_port = htons(443);
-
-    if (connect(webConn, (struct sockaddr *)&webServerAddr, sizeof(webServerAddr)) == -1)
+    // Start local -> remote data transfer
+    pid_t localToRemote = fork();
+    if (localToRemote == 0)
     {
-        perror("Error connecting to device web server");
-        // exit(EXIT_FAILURE);
-        return -1;
+        char buffer[BUFFER_SIZE];
+        ssize_t bytesRead;
+
+        while ((bytesRead = read(dst, buffer, sizeof(buffer))) > 0)
+        {
+            write(src, buffer, bytesRead);
+        }
+
+        close(src);
+        close(dst);
+        exit(EXIT_SUCCESS);
+    }
+    else if (localToRemote == -1)
+    {
+        perror("Error creating localToRemote process");
+        exit(EXIT_FAILURE);
     }
 
-    return webConn;
-}
-
-bool testWebConnection()
-{
-    int webConn = webConnection();
-    if (webConn == -1)
-    {
-        return false;
-    }
-
-    char *request = "GET / HTTP/1.1\r\nHost: 127.0.0.1\r\n\r\n";
-    if (send(webConn, request, strlen(request), 0) == -1)
-    {
-        perror("Error sending request to web server");
-        return false;
-    }
-
-    char buffer[1024];
-    int len = recv(webConn, buffer, sizeof(buffer) - 1, 0);
-    if (len == -1)
-    {
-        perror("Error receiving response from web server");
-        return false;
-    }
-
-    buffer[len] = '\0';
-    printf("Received response from web server: %s\n", buffer);
-
-    close(webConn);
-    return true;
+    // Wait for child processes to finish
+    int status;
+    waitpid(remoteToLocal, &status, 0);
+    waitpid(localToRemote, &status, 0);
 }
 
 int main()
 {
-    if (!testWebConnection())
-    {
-        printf("Error connecting to web server\n");
-        exit(EXIT_FAILURE);
-    }
-
     int conn = socket(AF_INET, SOCK_STREAM, 0);
     if (conn == -1)
     {
@@ -83,7 +72,7 @@ int main()
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = inet_addr("122.147.151.234");
-    serverAddr.sin_port = htons(27188);
+    serverAddr.sin_port = htons(48000);
 
     if (connect(conn, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
     {
@@ -93,91 +82,30 @@ int main()
 
     printf("Connected to server\n");
 
-    int epoll_fd = epoll_create(1);
-    if (epoll_fd == -1)
+    // Dial to device web server
+    int webConn = socket(AF_INET, SOCK_STREAM, 0);
+    if (webConn == -1)
     {
-        perror("Error creating epoll");
-        exit(EXIT_FAILURE);
-    }
-    struct epoll_event event;
-    event.events = EPOLLIN;
-    event.data.fd = conn;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, conn, &event) == -1)
-    {
-        perror("Error adding conn to epoll");
+        perror("Error creating webConn socket");
         exit(EXIT_FAILURE);
     }
 
+    struct sockaddr_in webServerAddr;
+    memset(&webServerAddr, 0, sizeof(webServerAddr));
+    webServerAddr.sin_family = AF_INET;
+    webServerAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    webServerAddr.sin_port = htons(443);
+
+    if (connect(webConn, (struct sockaddr *)&webServerAddr, sizeof(webServerAddr)) == -1)
+    {
+        perror("Error connecting to device web server");
+        exit(EXIT_FAILURE);
+    }
+
+    // Continuous proxy handling
     while (1)
     {
-        struct epoll_event events[1];
-        int n = epoll_wait(epoll_fd, events, 1, -1);
-        if (n == -1)
-        {
-            perror("Error waiting for epoll events");
-            exit(EXIT_FAILURE);
-        }
-
-        for (int i = 0; i < n; i++)
-        {
-            if (events[i].data.fd == conn)
-            {
-                char buffer[BUFFER_SIZE];
-                int len = recv(conn, buffer, BUFFER_SIZE, 0);
-                if (len == -1)
-                {
-                    perror("Error reading from conn");
-                    exit(EXIT_FAILURE);
-                }
-                if (len == 0)
-                {
-                    printf("Connection closed by server\n");
-                    exit(EXIT_SUCCESS);
-                }
-                printf("Received %d bytes from server\n", len);
-
-                int webConn = webConnection();
-                if (webConn == -1)
-                {
-                    printf("Error connecting to web server\n");
-                    continue;
-                }
-                printf("Connected to web server\n");
-
-                if (send(webConn, buffer, len, 0) == -1)
-                {
-                    perror("Error sending to web server");
-                    exit(EXIT_FAILURE);
-                }
-
-                printf("Sent %d bytes to web server\n", len);
-
-                while (1)
-                {
-                    len = recv(webConn, buffer, BUFFER_SIZE, 0);
-                    if (len == -1)
-                    {
-                        perror("Error reading from web server");
-                        exit(EXIT_FAILURE);
-                    }
-                    if (len == 0)
-                    {
-                        printf("Connection closed by web server\n");
-                        // exit(EXIT_SUCCESS);
-                        break;
-                    }
-                    printf("Received %d bytes from web server\n", len);
-
-                    if (send(conn, buffer, len, 0) == -1)
-                    {
-                        perror("Error sending to conn");
-                        exit(EXIT_FAILURE);
-                    }
-                    printf("Sent %d bytes to server\n", len);
-                }
-                close(webConn);
-            }
-        }
+        proxyConnection(conn, webConn);
     }
 
     return 0;
